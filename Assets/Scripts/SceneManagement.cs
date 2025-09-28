4e179e79ac08f148a7bfbee5287cba03e37cfb6d
Jo <joyce.cosmicgoblin@gmail.com>
2025-09-28 16:55:12 +0200

    LoadingScreen, Erz, working resources (hopefully)
    - fixed the resources, again
    - implemented all of our erz
    - made a working loading screen

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -1,10 +1,77 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using System.ComponentModel;
+using AsyncOperation = UnityEngine.AsyncOperation;
 
+// [ async loading from https://docs.unity3d.com/ScriptReference/AsyncOperation-allowSceneActivation.htm]
 public class SceneManagement : MonoBehaviour
 {
-    public void LoadInventoryScene()
+    [SerializeField] TextMeshProUGUI loadingText;
+    public GameObject startButton;
+    AsyncOperation asyncOperation;
+    public int sceneID;
+    public bool loadingInProgress = false;
+
+    public void Awake()
+    {
+        sceneID = SceneManager.GetActiveScene().buildIndex;
+
+        if (startButton != null && loadingText != null)
+        {
+            startButton.SetActive(false);
+            loadingText.text = "test";
+        }
+
+    }
+
+    public void Update()
+    {
+        if (!loadingInProgress) CheckScene();
+    }
+
+    public void CheckScene()
+    {
+        if (sceneID == 1)
+        {
+            StartCoroutine(WhileLoading());
+            loadingInProgress = true;
+        }
+    }
+    public void LoadingScreen()
     {
         SceneManager.LoadScene(1);      // TO-DO: async!
+    }
+
+
+
+    IEnumerator WhileLoading()
+    {
+        yield return null;
+
+        asyncOperation = SceneManager.LoadSceneAsync(2); 
+
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+            loadingText.text = "Loading: " + (asyncOperation.progress * 100) + "%";
+
+            if (asyncOperation.progress >=0.9f)
+            {
+                startButton.SetActive(true);
+                loadingText.text = "press play to play";
+            }
+
+            yield return null;
+        }
+    }
+
+    public void StartGame()
+    {
+        asyncOperation.allowSceneActivation = true;
+
     }
 }
